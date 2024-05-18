e062ca661820ede1e887faaacab727b7c2e7aa1d
Brayan Markov <145554659+BrayanMark@users.noreply.github.com>
2024-05-18 18:55:50 +0300

    Add files via upload

---
+++ Prototypes and Inheritance - Exercise/03.js
@@ -0,0 +1,18 @@
+function solve() {
+  function ExtensibleObject() {}
+
+  ExtensibleObject.prototype.extend = function (template) {
+    Object.entries(template).forEach(([key, value]) => {
+      if (value instanceof Function) {
+        Object.getPrototypeOf(this)[key] = value;
+      } else {
+        this[key] = value;
+      }
+    });
+  };
+
+  return new ExtensibleObject();
+}
+
+const myObj = solve();
+console.log(myObj);
